e22d3a5075f6f78b776aaaf5ae09db0ce6c71d10
Giovanni <giomadalozzo@gmail.com>
2021-09-13 18:07:40 -0300

    Adding level 1 basic assets

--- minigame/minigame/GameViewController.swift
+++ minigame/minigame/GameViewController.swift
@@ -10,19 +10,20 @@ import SpriteKit
 import GameplayKit
 
 class GameViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         if let view = self.view as! SKView? {
             // Load the SKScene from 'GameScene.sks'
-            if let scene = SKScene(fileNamed: "IntroScene") {
-                // Set the scale mode to scale to fit the window
-                scene.scaleMode = .aspectFill
-                
-                // Present the scene
-                view.presentScene(scene)
-            }
+            let scene = Level1()
+            // Set the scale mode to scale to fit the window
+            scene.scaleMode = .aspectFill
+            scene.size = view.frame.size
+            
+            // Present the scene
+            view.presentScene(scene)
+            
             
             view.ignoresSiblingOrder = true
             
@@ -30,11 +31,11 @@ class GameViewController: UIViewController {
             view.showsNodeCount = true
         }
     }
-
+    
     override var shouldAutorotate: Bool {
         return true
     }
-
+    
     override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
         if UIDevice.current.userInterfaceIdiom == .phone {
             return .allButUpsideDown
@@ -42,7 +43,7 @@ class GameViewController: UIViewController {
             return .all
         }
     }
-
+    
     override var prefersStatusBarHidden: Bool {
         return true
     }
--- minigame/minigame/Level1/Level1.swift
+++ minigame/minigame/Level1/Level1.swift
@@ -10,63 +10,85 @@ import GameplayKit
 
 class Level1: SKScene {
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+    private var player: SKSpriteNode = SKSpriteNode(imageNamed: "player")
+    private var enemy1: SKSpriteNode = SKSpriteNode(imageNamed: "enemy")
+    private var enemy2: SKSpriteNode = SKSpriteNode(imageNamed: "enemy")
+    private var soul: SKSpriteNode = SKSpriteNode(imageNamed: "soul")
+    
+    private var background: SKSpriteNode = SKSpriteNode(imageNamed: "background")
+    private var obstacleHorizontal: SKSpriteNode = SKSpriteNode(imageNamed: "obstacleHorizontal")
+    private var obstacleVertical: SKSpriteNode = SKSpriteNode(imageNamed: "obstacleVertical")
+    private var platform: SKSpriteNode = SKSpriteNode(imageNamed: "platform")
     
     override func didMove(to view: SKView) {
+        self.backgroundColor = UIColor.blue
+        self.anchorPoint = CGPoint(x: 0.5, y: 0.5)
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
-        
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
+        createBackground()
+        createPlayer()
     }
     
+    func createPlayer() {
+        player.position = CGPoint(x: 0, y: 0)
+        addChild(player)
+    }
+    
+    func createBackground(){
+        background.position = CGPoint(x: 0, y: 0)
+        background.size = CGSize(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height)
+        background.zPosition = 0
+        background.anchorPoint = CGPoint(x: 0.5, y: 0.5)
+        createPlatforms()
+        createObstacles()
+        createSouls()
+        createEnemies()
+        addChild(background)
+    }
+    
+    func createObstacles(){
+        obstacleVertical.position = CGPoint(x: 0, y: 0)
+        background.addChild(obstacleVertical)
+        
+        obstacleHorizontal.position = CGPoint(x: 0, y: 0)
+        background.addChild(obstacleHorizontal)
+    }
+    
+    func createEnemies(){
+        enemy1.position = CGPoint(x: 0, y: 0)
+        background.addChild(enemy1)
+        
+        enemy2.position = CGPoint(x: 0, y: 0)
+        background.addChild(enemy2)
+    }
+    
+    func createSouls(){
+        soul.position = CGPoint(x: 0, y: 0)
+        background.addChild(soul)
+    }
+    
+    func createPlatforms(){
+        platform.position = CGPoint(x: 0, y: 0)
+        background.addChild(platform)
+    }
+    
+    func attackAction(){
+        
+    }
     
     func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
+        
     }
     
     func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
+        
     }
     
     func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
+        
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
         
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
     }
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
